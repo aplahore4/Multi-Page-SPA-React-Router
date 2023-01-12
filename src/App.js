0c766f1e1b18b49504b8b4b8f6ce8b48da49eedf
ahmadPervaviz <aplahore4@gmail.com>
2023-01-12 19:25:07 +0500

    pages added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import AllQuotes from './pages/AllQuotes'
+import NewQuote from './pages/NewQuote'
+import NotFound from './pages/NotFound'
+import QuoteDetail from './pages/QuoteDetail'
+
 function App() {
-  return <div>Empty Canvas</div>
+  return (
+    <Layout>
+      <Routes>
+        <Route
+          path='/'
+          element={<AllQuotes />}
+          render={() => <Navigate to='/quotes' />}
+          end
+        />
+        <Route path='/quotes' element={<AllQuotes />} end />
+        <Route path='/quotes/:quoteId' element={<QuoteDetail />} />
+        <Route path='/new-quote' element={<NewQuote />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Layout>
+  )
 }
 
 export default App
