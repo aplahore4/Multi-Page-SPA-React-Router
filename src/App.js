7916da770d250289bd774de052e1ac648ff1437a
ahmadPervaviz <aplahore4@gmail.com>
2023-02-01 13:21:50 +0500

    comment router not working issue resolved.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<Navigate to='/quotes' replace />} end />
+        <Route path='/' element={<Navigate to='/quotes' replace />} />
         <Route path='/quotes' element={<AllQuotes />} end />
-        <Route path='/quotes/:quoteId' element={<QuoteDetail />} />
+        <Route path='/quotes/:quoteId/*' element={<QuoteDetail />} />
         <Route path='/new-quote' element={<NewQuote />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
